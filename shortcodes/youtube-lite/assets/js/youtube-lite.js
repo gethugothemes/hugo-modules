92d8e77ce247153c27522a67908f2043ef005307
Sojon <sojon.themefisher@gmail.com>
2022-04-19 10:50:03 +0600

    yt-lite button style

--- shortcodes/youtube-lite/assets/js/youtube-lite.js
+++ shortcodes/youtube-lite/assets/js/youtube-lite.js
@@ -107,18 +107,22 @@ class LiteYTEmbed extends HTMLElement {
       #playButton {
         width: 70px;
         height: 46px;
-        background-color: var(--lyt-play-btn-hover);
         z-index: 1;
-        opacity: 0.8;
+        opacity: 0.9;
         border-radius: 14%;
         transition: var(--lyt-animation);
         border: 0;
         cursor:pointer;
+        background-image: url(data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='100%25' version='1.1' viewBox='0 0 68 48' width='100%25'%3E%3Cpath class='ytp-large-play-button-bg' d='M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z' fill='%23f00'%3E%3C/path%3E%3Cpath d='M 45,24 27,14 27,34' fill='%23fff'%3E%3C/path%3E%3C/svg%3E);
+        background-repeat: no-repeat;
+        background-position: center;
+        filter: grayscale(1);
       }
 
       #frame:hover > #playButton {
-        background-color: var(--lyt-play-btn-hover);
+        background-image: url(data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='100%25' version='1.1' viewBox='0 0 68 48' width='100%25'%3E%3Cpath class='ytp-large-play-button-bg' d='M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z' fill='%23f00'%3E%3C/path%3E%3Cpath d='M 45,24 27,14 27,34' fill='%23fff'%3E%3C/path%3E%3C/svg%3E);
         opacity: 1;
+        filter: grayscale(0);
       }
 
       #playButton:before {
