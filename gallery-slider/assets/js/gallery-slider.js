11cd7df9dbc013476ecc592fd26aff310ec42dad
somrat sorkar <themefisher.dev@gmail.com>
2023-05-28 10:13:07 +0600

    modified gallery-slider

--- gallery-slider/assets/js/gallery-slider.js
+++ gallery-slider/assets/js/gallery-slider.js
@@ -16,18 +16,21 @@
   }, 200);
 
   // gallery slider
-  new Swiper(".gallery-slider", {
-    slidesPerView: 1,
-    loop: true,
-    autoHeight: true,
-    spaceBetween: 0,
-    speed: 1500,
-    autoplay: {
-      delay: 5000,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  var isGallerySlider = document.getElementsByClassName("gallery-slider");
+  if (isGallerySlider.length > 0) {
+    new Swiper(".gallery-slider", {
+      slidesPerView: 1,
+      loop: true,
+      autoHeight: true,
+      spaceBetween: 0,
+      speed: 1500,
+      autoplay: {
+        delay: 5000,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
 })();
