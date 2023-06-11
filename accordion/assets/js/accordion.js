2346ce88e23378934c04a5024f71c3641772a301
Sojon <sojon.themefisher@gmail.com>
2023-06-11 12:14:40 +0600

    updated accordion script & style

--- accordion/assets/js/accordion.js
+++ accordion/assets/js/accordion.js
@@ -1,12 +1,31 @@
 // accordion script
 (function () {
   "use strict";
-
-  const accordions = document.querySelectorAll("[data-accordion]");
-  accordions.forEach((header) => {
-    header.addEventListener("click", () => {
-      const accordionItem = header.parentElement;
-      accordionItem.classList.toggle("active");
+  window.addEventListener('DOMContentLoaded', () => {
+    const accordionContents = document.querySelectorAll('.accordion-content');
+    accordionContents.forEach(content => {
+      const contentHeight = content.scrollHeight;
+      content.style.setProperty('--max-height', `${contentHeight}px`);
     });
+
+    const accordions = document.querySelectorAll("[data-accordion]");
+    accordions.forEach((header) => {
+      header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const isActive = accordionItem.classList.contains("active");
+
+        // Remove active class from other accordion items
+        accordions.forEach((otherHeader) => {
+          const otherAccordionItem = otherHeader.parentElement;
+          if (otherAccordionItem !== accordionItem) {
+            otherAccordionItem.classList.remove("active");
+          }
+        });
+
+        // Toggle active class for the clicked accordion item
+        accordionItem.classList.toggle("active", !isActive);
+      });
+    });
+    // end of toggling active-class
   });
 })();
