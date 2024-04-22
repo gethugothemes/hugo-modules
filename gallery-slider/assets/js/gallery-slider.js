7f85636d02b7585ce7a6368393e97e62addadc40
Somrat <58769763+tfsomrat@users.noreply.github.com>
2024-04-22 09:23:16 +0600

    Revert "feat: Refactored gallery to be pure CSS. Fixes JS bug where code would gallery would only resize on scroll"

--- gallery-slider/assets/js/gallery-slider.js
+++ gallery-slider/assets/js/gallery-slider.js
@@ -3,6 +3,35 @@
   // gallery init
   GLightbox();
 
+  const elementIsVisibleInViewport = (el, partiallyVisible = true) => {
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return partiallyVisible
+      ? ((top > 0 && top < innerHeight) ||
+        (bottom > 0 && bottom < innerHeight)) &&
+      ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+      : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+  };
+
+  const justify_scale = screen.height * 0.25;
+  const allGallery = document.querySelectorAll(".gallery");
+  const checkVisibility = () => {
+    allGallery.forEach((gallery) => {
+      const items = gallery.querySelectorAll(".gallery-item");
+      if (elementIsVisibleInViewport(gallery)) {
+        items.forEach((item) => {
+          let image = item.querySelector("img");
+          let ratio = image.naturalWidth / image.naturalHeight;
+          item.style.width = justify_scale * ratio + "px";
+          item.style.flexGrow = ratio;
+          image.style.opacity = 1;
+        });
+      }
+    });
+  }
+  window.addEventListener("scroll", checkVisibility);
+  window.addEventListener("load", checkVisibility);
+
   // gallery slider
   var isGallerySlider = document.getElementsByClassName("gallery-slider");
   if (isGallerySlider.length > 0) {
